2839998444f8166e5e2106079739f899d596f024
pc52 <nyabutofelix@outlook.com>
2025-03-15 16:11:47 +0100

    Cleaning up

--- back/server.py
+++ back/server.py
@@ -14,13 +14,11 @@ def connect():
         password = os.getenv("DB_PASSWORD"),
         database = os.getenv("DB_NAME"),
         )
-        print("Connection established!")  # For debugging purposes only
+        # print("Connection established!")  # For debugging purposes only
         return connection
 
     except Error as err:
-        print(f"Something went wrong.[Error at server.py]: {err}")
-
-    return None  # Explicitly return None if the connection fails
+        return (f"Something went wrong.[Error at server.py]: {err}")
 
 def connect4seed():
     # Establishes a connection to the database.
@@ -31,10 +29,8 @@ def connect4seed():
         user = os.getenv("DB_USER"),
         password = os.getenv("DB_PASSWORD"),
         )
-        print("Connection established!")  # For debugging purposes only
+        # print("Connection established!")  # For debugging purposes only
         return connection
 
     except Error as err:
-        print(f"Something went wrong.[Error at server.py]: {err}")
-
-    return None  # Explicitly return None if the connection fails
+        return (f"Something went wrong.[Error at server.py]: {err}")
